5abb6914698a1cb2ba7cf92268bf4ffc4e965f6f
John Pinares <biscuitduke@yahoo.com>
2023-12-08 17:52:30 -0500

    day 4 part 1

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -6,11 +6,10 @@ f = np.loadtxt(f[:,1], dtype="U", delimiter='|')
 winning_cards = np.char.split(f[:,0])
 my_nums = (np.char.split(f[:,1]))
 
-total = np.empty(len(my_nums))
+total = np.zeros(len(my_nums))
 for i in range(len(my_nums)):
 	matches = len(np.intersect1d(winning_cards[i], my_nums[i]))
 	if matches:
 		total[i] = math.pow(2,matches-1)
-	else:
-		total[i] = 0
+
 print(np.sum(total))
