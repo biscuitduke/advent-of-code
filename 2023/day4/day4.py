c67ed27d9c4bb5f1f719db4fb832e0ecf9f832a8
John Pinares <biscuitduke@yahoo.com>
2023-12-10 12:07:04 -0500

    day 4 - part 2 done

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -6,10 +6,27 @@ f = np.loadtxt(f[:,1], dtype="U", delimiter='|')
 winning_cards = np.char.split(f[:,0])
 my_nums = (np.char.split(f[:,1]))
 
-total = np.zeros(len(my_nums))
-for i in range(len(my_nums)):
-	matches = len(np.intersect1d(winning_cards[i], my_nums[i]))
-	if matches:
-		total[i] = math.pow(2,matches-1)
+# total1 = np.zeros(len(my_nums))
+total2 = np.zeros((len(my_nums)+1, len(my_nums)+1))
+matches = np.zeros((len(my_nums)+1,1))
+for i in range(1,len(my_nums)+1):
+	matches[i] = len(np.intersect1d(winning_cards[i-1], my_nums[i-1]))
+# 	if matches[i,0]:
+# 		total1[i] = math.pow(2,matches[i,0]-1)
 
-print(np.sum(total))
+for i in range(1,len(my_nums)+1):
+	for j in range(1,len(my_nums)+1):
+		if i==j:
+			total2[i,j] = total2[i,j-1] + 1
+			total2[i+1:int(matches[i][0])+i+1,j] = 1#total2[i,j] + total2[i,j-1]
+			total2[i,j:] = total2[i,j]
+		elif total2[i,j] == 0:
+			total2[i,j] = total2[i,j-1]
+		elif total2[i,j] != 0:
+			# total2[i,j:] = total2[i,j]
+			total2[i,j] = total2[i,j-1] + total2[j,j]
+
+print(np.sum(total2[:,j]))
+
+# print(np.sum(total1))
+
