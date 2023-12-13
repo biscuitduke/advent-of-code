a0a3832dd167c89a71192a2dc9d0255da76040d0
biscuitduke <biscuitduke@yahoo.com>
2023-12-13 18:59:29 +0000

    day 6

--- 2023/day6/day6-2.py
+++ 2023/day6/day6-2.py
@@ -1,4 +1,4 @@
-f = open('example', 'r').read()
+f = open('input', 'r').read()
 Times = f.split('\n')[0].split(':')[1]
 Times = int(Times.replace(' ', ''))
 # Times = [int(x) for x in Times if x]
@@ -17,22 +17,22 @@ for i in range(race_num):
     min_time = 0
     wins = 0
     for time_button_is_pushed in range(Times[i], -1, -1):
-        print(time_button_is_pushed)
+        # print(time_button_is_pushed)
         speed = time_button_is_pushed
         time_traveled = Times[i] - time_button_is_pushed
         distance_traveled = speed * time_traveled
         if distance_traveled > Distances[i]:
             max_time = time_button_is_pushed
-            print(max_time)
+            # print(max_time)
             break
     for time_button_is_pushed in range(Times[i]):
-        print(time_button_is_pushed)
+        # print(time_button_is_pushed)
         speed = time_button_is_pushed
         time_traveled = Times[i] - time_button_is_pushed
         distance_traveled = speed * time_traveled
         if distance_traveled > Distances[i]:
             min_time = time_button_is_pushed
-            print(min_time)
+            # print(min_time)
             break
             
     
