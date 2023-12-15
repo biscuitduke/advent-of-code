6ab9c45ae91d7efe2a9432fe617b469f4ec0dbe5
biscuitduke <biscuitduke@yahoo.com>
2023-12-15 18:24:50 +0000

    day 7 part 2

--- 2023/day7/day7.py
+++ 2023/day7/day7.py
@@ -5,18 +5,18 @@ Cards = {
     "A": 14,
     "K": 13,
     "Q": 12,
-    "J": 11,
-    "T": 10,
-    "9": 9,
-    "8": 8,
-    "7": 7,
-    "6": 6,
-    "5": 5,
-    "4": 4,
-    "3": 3,
-    "2": 2
+    "T": 11,
+    "9": 10,
+    "8": 9,
+    "7": 8,
+    "6": 7,
+    "5": 6,
+    "4": 5,
+    "3": 4,
+    "2": 3,
+    "J": 2
 }
-file = 'example'
+file = 'input'
 with open(file) as f:
   Hands = [k for line in f for k, v in [line.strip().split()]]
 with open(file) as f:
@@ -33,34 +33,42 @@ high_card = []
 for hand in Hands:
   power = 0
   count = 0
-  max_card = ''
+  card_cnt=0
   for card in hand:
-    card_cnt = Counter(hand)[card]
+    if card != 'J':
+      card_cnt = Counter(hand)[card]
     if card_cnt > count:
       count = card_cnt
+  print(count)
+  hand_set = set(hand)
+  isJ = False
+  if 'J' in hand_set:
+    count +=Counter(hand)['J']
+    isJ = True
+  print(hand, count)
   num_of_cards = len(set(hand))
-  if num_of_cards == 1:
+  if num_of_cards == 1 or (num_of_cards == 2 and isJ):
     five_of_a_kind.append(hand)
-  if num_of_cards == 2:
+  elif num_of_cards == 2 or (num_of_cards == 3 and isJ):
     if count == 4:
       four_of_a_kind.append(hand)
     elif count == 3:
       full_house.append(hand)
-  if num_of_cards == 3:
+  elif num_of_cards == 3 or (num_of_cards == 4 and isJ):
     if count == 3:
       three_of_a_kind.append(hand)
     elif count == 2:
       two_pair.append(hand)
-  if num_of_cards == 4:
+  elif num_of_cards == 4 or (num_of_cards == 5 and isJ):
     one_pair.append(hand)
-  if num_of_cards == 5:
+  elif num_of_cards == 5 and not isJ:
     high_card.append(hand)
 
 hand_types = [
     five_of_a_kind, four_of_a_kind, full_house, three_of_a_kind, two_pair,
     one_pair, high_card
 ]
-
+# print(hand_types)
 sorted_list = []
 for hand_type in hand_types:
   hand_type = sorted(hand_type,
@@ -72,6 +80,8 @@ for hand_type in hand_types:
 sorted_list.reverse()
 enumerated_list = list(enumerate(sorted_list, 1))
 sum = 0
+for i in enumerated_list:
+  print(i)
 for hand in enumerated_list:
   sum += hand[0] * Bids.get(hand[1])
 print(sum)
