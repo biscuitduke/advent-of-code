c71cd588cf7c44cd62aa46a9d1e6f41462f7a953
John Pinares <biscuitduke@yahoo.com>
2023-12-16 14:07:40 -0500

    day 8 part 1 done

--- 2023/day8/day8.py
+++ 2023/day8/day8.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 #result_dict = {pattern.search(s).group(1):tuple(map(str.strip, pattern.search(s).group(2).split(',')))}
 import re
 
@@ -15,7 +16,7 @@ for line in Lines[2:]:
   network[pattern.search(line).group(1)] = tuple(map(str.strip, pattern.search(line).group(2).split(',')))
 # print(network)
 
-current_node = list(network.keys())[0]
+current_node = 'AAA'
 steps = 0
 while current_node != 'ZZZ':
   for c in instructions:
@@ -24,5 +25,5 @@ while current_node != 'ZZZ':
     if c == 'L':
       current_node = network[current_node][0]
     steps +=1
-    print(current_node, c)
+    # print(current_node, c)
 print(steps)
