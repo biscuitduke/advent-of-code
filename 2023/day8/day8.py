e94bb7ab9b528d23ae3f6396272da7f14c4b6a84
John <biscuitduke@yahoo.com>
2023-12-17 11:14:52 -0500

    finished day 8 part 2

--- 2023/day8/day8.py
+++ 2023/day8/day8.py
@@ -16,14 +16,24 @@ for line in Lines[2:]:
   network[pattern.search(line).group(1)] = tuple(map(str.strip, pattern.search(line).group(2).split(',')))
 # print(network)
 
-current_node = 'AAA'
-steps = 0
-while current_node != 'ZZZ':
-  for c in instructions:
-    if c == 'R':
-      current_node = network[current_node][1]
-    if c == 'L':
-      current_node = network[current_node][0]
-    steps +=1
-    # print(current_node, c)
-print(steps)
+starting_nodes = [x for x in network.keys() if x[2] == 'A']
+ending_nodes = [x for x in network.keys() if x[2] == 'Z']
+distances = []
+for current_node in starting_nodes:
+  steps = 0
+  while current_node not in ending_nodes:
+    for c in instructions:
+      if c == 'R':
+        current_node = network[current_node][1]
+      if c == 'L':
+        current_node = network[current_node][0]
+      steps +=1
+      # print(current_node, c)
+  distances.append(steps)
+
+from math import gcd
+#will work for an int array of any length
+lcm = 1
+for i in distances:
+    lcm = lcm*i//gcd(lcm, i)
+print(lcm)
